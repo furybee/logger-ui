f5f32f0a95dede7ff7e1645b394c557a5089066b
sebastienfontaine <hello@sebastienfontaine.me>
2022-06-05 18:22:28 +0200

    added simple responsive

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <!-- Meta Information -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="robots" content="noindex, nofollow" />
+<html lang="en" class="h-full">
+<head>
+    <!-- Meta Information -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="robots" content="noindex, nofollow" />
 
-        <!-- Title -->
-        <title>Logger UI - {{ config('logger-ui.project') }} - {{ config('logger-ui.environment') }}</title>
+    <!-- Title -->
+    <title>Logger UI - {{ config('logger-ui.project') }} - {{ config('logger-ui.environment') }}</title>
 
-        <!-- Style sheets -->
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <!-- Style sheets -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
 
-        <link href="{{ asset(mix('app.css', 'vendor/logger-ui')) }}" rel="stylesheet" type="text/css" />
-        <link rel="icon" type="image/png" href="{{ asset('vendor/logger-ui/favicon.png') }}" />
-    </head>
-    <body class="bg-gray-900">
-        <div id="logger-ui" class="main">
-            <router-view></router-view>
-        </div>
+    <link href="{{ asset(mix('app.css', 'vendor/logger-ui')) }}" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="image/png" href="{{ asset('vendor/logger-ui/favicon.png') }}" />
+</head>
+<body class="bg-gray-900 h-full">
+<div id="logger-ui" class="main h-full">
+    <router-view></router-view>
+</div>
 
-        <script src="{{ asset(mix('app.js', 'vendor/logger-ui')) }}"></script>
-    </body>
+<script src="{{ asset(mix('app.js', 'vendor/logger-ui')) }}"></script>
+</body>
 </html>
